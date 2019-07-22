d0f50df64871260f3fea04895c36420a9f0ed679
LTHLTH <xoghk0321@gmail.com>
2019-07-22 22:29:35 +0900

    requestSecret & createAccount Mutation 수정

--- src/Routes/Auth/AuthContainer.js
+++ src/Routes/Auth/AuthContainer.js
@@ -10,6 +10,7 @@ export default () => {
   const username = useInput("");
   const firstName = useInput("");
   const lastName = useInput("");
+  const secret = useInput("");
   const email = useInput("xoghk0321@gmail.com");
   const requestSecretMutation = useMutation(LOG_IN, {
     variables: { email: email.value }
@@ -29,10 +30,15 @@ export default () => {
     if (action === "logIn") {
       if (email.value !== "") {
         try {
-          const { requestSecret } = await requestSecretMutation();
+          const {
+            data: { requestSecret }
+          } = await requestSecretMutation();
           if (!requestSecret) {
             toast.error("You dont have an account yet, create one");
             setTimeout(() => setAction("signUp"), 3000);
+          } else {
+            toast.success("Check your inbox for your login secret");
+            setAction("confirm");
           }
         } catch {
           toast.error("Can't request secret, try again");
@@ -48,7 +54,9 @@ export default () => {
         lastName.value !== ""
       ) {
         try {
-          const { createAccount } = await createAccountMutation();
+          const {
+            data: { createAccount }
+          } = await createAccountMutation();
           if (!createAccount) {
             toast.error("Can't create account");
           } else {
@@ -72,6 +80,7 @@ export default () => {
       firstName={firstName}
       lastName={lastName}
       email={email}
+      secret={secret}
       onSubmit={onSubmit}
     />
   );
